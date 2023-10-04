0f351418a1d1362095f9ed576372d48f758c162e
rafa.saez@flywire.com <rafa.saez@flywire.com>
2023-10-04 12:33:21 +0200

    Remove duplicated argument keepers

--- main.tf
+++ main.tf
@@ -12,9 +12,6 @@ resource "random_id" "salt" {
     redis_version = "${var.redis_version}"
   }
   byte_length = 8
-  keepers = {
-    redis_version = var.redis_version
-  }
 }
 
 resource "aws_elasticache_replication_group" "redis" {
