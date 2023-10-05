538f45b330b04b614506c10d1327e655518c4550
rafa.saez@flywire.com <rafa.saez@flywire.com>
2023-10-05 12:06:45 +0200

    Update parameter naming

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_vpc" "vpc" {
 locals {
   vpc_name = lookup(data.aws_vpc.vpc.tags, "Name", var.vpc_id)
   major_redis_version = parseint(substr(var.redis_version, 0,1),10)
-  parameter_group_family = local.major_redis_version < 6 ?  "redis${replace(var.redis_version, "/\\.[\\d]+$/", "")}" : local.major_redis_version == 6 ? "redis${replace(var.redis_version, "/\\.[\\d|x]+$/", "")}.x" : "redis${local.major_redis_version}"
+  parameter_group_family = local.major_redis_version < 6 ? "redis${replace(var.redis_version, "/\\.[\\d]+$/", "")}" : local.major_redis_version == 6 ? "redis${replace(var.redis_version, "/\\.[\\d|x]+$/", "")}.x" : "redis${local.major_redis_version}"
   }
 
 resource "random_id" "salt" {
@@ -17,12 +17,12 @@ resource "random_id" "salt" {
 
 resource "aws_elasticache_replication_group" "redis" {
   replication_group_id          = format("%.20s", "${var.name}-${var.env}")
-  replication_group_description = "Terraform-managed ElastiCache replication group for ${var.name}-${var.env}-${local.vpc_name}"
-  number_cache_clusters         = var.redis_clusters
+  description = "Terraform-managed ElastiCache replication group for ${var.name}-${var.env}-${local.vpc_name}"
+  num_cache_clusters            = var.redis_clusters
   node_type                     = var.redis_node_type
   automatic_failover_enabled    = var.redis_failover
   auto_minor_version_upgrade    = var.auto_minor_version_upgrade
-  availability_zones            = var.availability_zones
+  preferred_cache_cluster_azs   = var.availability_zones
   multi_az_enabled              = var.multi_az_enabled
   engine                        = "redis"
   at_rest_encryption_enabled    = var.at_rest_encryption_enabled
